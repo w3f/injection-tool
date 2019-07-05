7e8224e48793db1ccc3676fc257bb6d36ce8d76c
Logan Saether <x@logansaether.com>
2019-07-05 09:39:46 +0200

    Better logging

--- src/injection.ts
+++ src/injection.ts
@@ -27,6 +27,7 @@ export const injectAllocations = async (
   addresses: string[],
   balances: string[],
   txParams: TxParams,
+  noisy: boolean = true,
 ) => {
 
   if (addresses.length != balances.length) {
@@ -35,7 +36,22 @@ export const injectAllocations = async (
 
   let i = 0;
   while (i < addresses.length) {
-    frozenToken.methods.transfer(addresses[i], balances[i]).send(txParams);
+    if (noisy) {
+      console.log(`Sending transfer of ${balances[i]} FrozenToken to ${addresses[i]}`);
+    }
+
+    const txResult = frozenToken.methods.transfer(addresses[i], balances[i]).send(txParams);
+    
+    if (!txResult.status) {
+      console.error(`Transaction to ${addresses[i]} FAILED! Hash:
+      ${txResult.transactionHash}`);
+    } else {
+      if (noisy) {
+        console.log(`Transfer to ${addresses[i]} succeeded. Hash:
+        ${txResult.transactionHash}`);
+      }
+    }
+
     i++;
   }
 }
@@ -46,6 +62,7 @@ export const injectAmends = async (
   amends: string[], 
   txParams: TxParams, 
   step: number = 50,
+  noisy: boolean = true,
 ): Promise<boolean> => {
   if (originals.length != amends.length) {
     throw new Error('Passed two arrays of different lengths.');
@@ -55,10 +72,25 @@ export const injectAmends = async (
 
   let promises = [];
   for (let i = 0; i <= amends.length; i += step) {
+    if (noisy) {
+      console.log(`Amends | i: ${i} | step: ${step} | Sending...`);
+    }
+
     const originalsArg = originals.slice(i, step);
     const amendsArg = amends.slice(i, step);
 
-    const txPromise = claims.methods.amend(originalsArg, amendsArg).send(txParams);
+    const txPromise = claims.methods.amend(originalsArg, amendsArg).send(txParams)
+    .on('receipt', (receipt: any) => {
+      if (!receipt.status) {
+        console.error(`Amends | i: ${i} | step: ${step} | FAILED`);
+      } else {
+        if (noisy) {
+          console.log(`Amends | i: ${i} | step: ${step} | Succeeded
+  Hash: ${receipt.transactionHash}`);
+        }
+      }
+    });
+
     promises.push(txPromise);
 
     step = Math.min(step * 2, amends.length-1);
@@ -76,15 +108,31 @@ export const injectIndices = async (
   txParams: TxParams,
   start: number = 0,
   step: number = 50,
+  noisy: boolean = true,
 ): Promise<boolean> => {
 
   step = Math.min(step, addresses.length-1);
 
   let promises = [];
   for (let i = start; i < addresses.length-1; i += step) {
+    if (noisy) {
+      console.log(`Indices | i: ${i} | step: ${step} | Sending...`);
+    }
+
     const indicesArg = addresses.slice(i, step);
 
-    const txPromise = claims.methods.assignIndices(indicesArg).send(txParams);
+    const txPromise = claims.methods.assignIndices(indicesArg).send(txParams)
+    .on('receipt', (receipt: any) => {
+      if (!receipt.status) {
+        console.error(`Indices | i: ${i} | step: ${step} | FAILED`);
+      } else {
+        if (noisy) {
+          console.log(`Indices | i: ${i} | step: ${step} | Succeeded
+  Hash: ${receipt.transactionHash}`);
+        }
+      }
+    });
+    
     promises.push(txPromise);
 
     step = Math.min(step * 2, addresses.length-1);
@@ -101,15 +149,31 @@ export const injectVesting = async (
   txParams: TxParams,
   start: number = 0,
   step: number = 50,
+  noisy: boolean = true,
 ): Promise<boolean> => {
 
   step = Math.min(step, addresses.length-1);
 
   let promises = [];
   for (let i = start; i < addresses.length-1; i += step) {
+    if (noisy) {
+      console.log(`Vesting | i: ${i} | step: ${step} | Sending...`);
+    }
+
     const vestingArg = addresses.slice(i, step);
 
-    const txPromise = claims.methods.setVesting(vestingArg).send(txParams);
+    const txPromise = claims.methods.setVesting(vestingArg).send(txParams)
+    .on('receipt', (receipt: any) => {
+      if (!receipt.status) {
+        console.error(`Vesting | i: ${i} | step: ${step} | FAILED`);
+      } else {
+        if (noisy) {
+          console.log(`Vesting | i: ${i} | step: ${step} | Succeeded
+  Hash: ${receipt.transactionHash}`);
+        }
+      }
+    });
+    
     promises.push(txPromise);
 
     step = Math.min(step * 2, addresses.length-1);
