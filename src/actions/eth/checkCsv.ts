1ca91fb8ebb6a933d4ba30d826cf02ef6635e23d
Logan Saether <x@logansaether.com>
2020-05-21 07:04:03 +0200

    Checking script

--- src/actions/eth/checkCsv.ts
+++ src/actions/eth/checkCsv.ts
@@ -1,9 +1,7 @@
-import { Command } from "commander";
 import * as fs from "fs";
 import Web3 from "web3";
-import { initFrozenToken } from "./dotAllocations";
 
-const w3Util = new Web3().utils;
+import { convertFromDecimalString } from "./vesting";
 
 const Claims = require("../../../build/contracts/Claims.json");
 const FrozenToken = require("../../../build/contracts/FrozenToken.json");
@@ -50,7 +48,7 @@ type Options = {
 export const checkCsv = async (opts: Options) => {
   const { claimAddress, csvFileType, csv, frozenTokenAddress, providerUrl } = opts;
 
-  const frozenToken = await initFrozenToken(frozenTokenAddress, providerUrl);
+  const frozenToken = await initToken(frozenTokenAddress, providerUrl);
   const claims = await initClaims(claimAddress, providerUrl)
 
   const fileType = getCsvFileType(csvFileType);
@@ -97,22 +95,82 @@ export const checkCsv = async (opts: Options) => {
     case CsvFileType.IncreaseVested: {
       const file =fs.readFileSync(csv, { encoding: "utf-8" });
       let counter = 0;
-      let error = 0;
+      let errors = 0;
       for (const line of file.split("\n").filter(line => line !== "")) {
         const [ethAddr, amt] = line.split(",");
+        const amount = convertFromDecimalString(amt);
         const result = await claims.methods.claims(ethAddr).call();
         console.log(result);
+        if (result.vested.toString() !== amount) {
+          console.log(`|${counter}| Failed! ADDRESS ${ethAddr} GOT ${result.vested.toString()} EXPECTED ${amount}`);
+          errors++;
+        } else {
+          console.log(`|${counter}| OK`);
+        }
+        counter++;
       }
-
+      console.log(`DONE\nERRORS FOUND: ${errors}`);
       break;
     }
     case CsvFileType.Indices: {
+      const file = fs.readFileSync(csv, { encoding: "utf-8" });
+      let counter = 0;
+      let errors = 0;
+      for (const line of file.split("\n").filter(line => line !== "")) {
+        const ethAddr = line;
+        const result = await claims.methods.claims(ethAddr).call();
+        console.log(result);
+        if (Number(result.index) !== counter) {
+          console.log(`|${counter}| Failed! ADDRESS ${ethAddr} GOT ${Number(result.index)} EXPECTED ${counter}`);
+          errors++;
+        } else {
+          console.log(`|${counter}| OK`);
+        }
+        counter++;
+      }
+      console.log(`DONE\nERRORS FOUND: ${errors}`);
+
       break;
     }
     case CsvFileType.Regular: {
+      const file = fs.readFileSync(csv, { encoding: "utf-8" });
+      let counter = 0;
+      let errors = 0;
+      for (const line of file.split("\n").filter(line => line !== "")) {
+        const [ethAddr, amt] = line.split(",");
+        const amount = convertFromDecimalString(amt);
+        const result = await frozenToken.methods.balanceOf(ethAddr).call();
+        console.log(result);
+        if (result.toString() !== amount) {
+          console.log(`|${counter}| Failed! ADDRESS ${ethAddr} GOT ${result.toString()} EXPECTED ${amount}`);
+          errors++;
+        } else {
+          console.log(`|${counter}| OK`);
+        }
+        counter++;
+      }
+      console.log(`DONE\nERRORS FOUND: ${errors}`);
+
       break;
     }
     case CsvFileType.Vested: {
+      const file = fs.readFileSync(csv, { encoding: "utf-8" });
+      let counter = 0;
+      let errors = 0;
+      for (const line of file.split("\n").filter(line => line !== "")) {
+        const [ethAddr,amt] = line.split(",");
+        const amount = convertFromDecimalString(amt);
+        const result = await claims.methods.claims(ethAddr).call();
+        console.log(result);
+        if (result.vested.toString() !== amount) {
+          console.log(`|${counter}| Failed! ADDRESS ${ethAddr} GOT ${result.vested.toString()} EXPECTED ${amount}`);
+          errors++;
+        } else {
+          console.log(`|${counter}| OK`);
+        }
+        counter++;
+      }
+      console.log(`DONE\nERRORS FOUND: ${errors}`);
       break;
     }
     default: throw Error("Should never reach here. Something went wrong.")
